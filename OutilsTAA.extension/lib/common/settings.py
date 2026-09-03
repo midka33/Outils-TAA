56983e97581731c5e1cc8e1249ef5cdd6ff500db
midka33 <akdim@taa.archi>
2026-09-03 11:19:49 +0200

    refactor: add common settings contract

---
+++ OutilsTAA.extension/lib/common/settings.py
@@ -0,0 +1,14 @@
+"""Gestion des préférences locales Outils TAA.
+
+Le stockage concret sera branché sur le mécanisme retenu par le projet.
+"""
+
+
+class SettingsStore(object):
+    """Interface minimale pour les préférences persistantes."""
+
+    def get(self, key, default=None):
+        raise NotImplementedError
+
+    def set(self, key, value):
+        raise NotImplementedError
