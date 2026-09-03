1c6a2749a442510e992ac09ddd565f62a89e33ac
midka33 <akdim@taa.archi>
2026-09-03 11:19:59 +0200

    refactor: add common parameter utilities

---
+++ OutilsTAA.extension/lib/common/parameter_utils.py
@@ -0,0 +1,19 @@
+"""Lecture et validation des paramètres Revit."""
+
+
+def get_parameter(element, name):
+    """Retourne le paramètre demandé ou None."""
+    if element is None or not name:
+        return None
+    return element.LookupParameter(name)
+
+
+def get_parameter_value(element, name, default=None):
+    """Retourne la valeur brute exploitable d'un paramètre."""
+    parameter = get_parameter(element, name)
+    if parameter is None:
+        return default
+    try:
+        return parameter.AsValueString() or parameter.AsString() or default
+    except Exception:
+        return default
