1ef6df3f07ea6ed61c52ee4fddef3c69c2e0f3a8
midka33 <akdim@taa.archi>
2026-09-03 11:20:04 +0200

    refactor: add common transaction adapter

---
+++ OutilsTAA.extension/lib/common/transaction.py
@@ -0,0 +1,29 @@
+"""Gestion centralisée des transactions Revit."""
+
+try:
+    from Autodesk.Revit.DB import Transaction
+except ImportError:
+    Transaction = None
+
+
+class RevitTransaction(object):
+    """Gestionnaire de contexte pour une transaction courte et explicite."""
+
+    def __init__(self, document, name):
+        self.document = document
+        self.name = name
+        self._transaction = None
+
+    def __enter__(self):
+        if Transaction is None:
+            raise RuntimeError("L'API Revit n'est pas disponible.")
+        self._transaction = Transaction(self.document, self.name)
+        self._transaction.Start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None:
+            self._transaction.Commit()
+        else:
+            self._transaction.RollBack()
+        return False
