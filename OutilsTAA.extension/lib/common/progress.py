a093de580f2b83f338fb9fcb46109f1401cff59a
midka33 <akdim@taa.archi>
2026-09-03 11:20:07 +0200

    refactor: add common progress contract

---
+++ OutilsTAA.extension/lib/common/progress.py
@@ -0,0 +1,11 @@
+"""Contrat commun de progression."""
+
+
+class ProgressReporter(object):
+    """Abstraction minimale pour permettre une UI de progression uniforme."""
+
+    def report(self, current, total, message=""):
+        raise NotImplementedError
+
+    def is_cancelled(self):
+        return False
