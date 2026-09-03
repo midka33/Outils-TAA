07e28cae8789504af74864ae585d937dbac8ce98
midka33 <akdim@taa.archi>
2026-09-03 18:03:26 +0200

    Export: ajouter le modèle de dossier de publication

---
+++ OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_folder.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Modèle métier d'un dossier de publication Export."""
+
+
+class PublicationFolder(object):
+    """Dossier persistant contenant des carnets de publication."""
+
+    def __init__(self, name, folder_id=None, parent_id=None, persistent=True):
+        self.id = folder_id
+        self.name = name
+        self.parent_id = parent_id
+        self.persistent = persistent
