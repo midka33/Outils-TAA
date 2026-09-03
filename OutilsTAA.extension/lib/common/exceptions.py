29f8659f944eb1f52a1518588794fd88c117df27
midka33 <akdim@taa.archi>
2026-09-03 11:18:45 +0200

    refactor: add common exceptions

---
+++ OutilsTAA.extension/lib/common/exceptions.py
@@ -0,0 +1,13 @@
+"""Exceptions communes à Outils TAA."""
+
+
+class OutilsTAAError(Exception):
+    """Erreur racine contrôlée de l'application."""
+
+
+class ValidationError(OutilsTAAError):
+    """Donnée ou état métier invalide."""
+
+
+class ExportError(OutilsTAAError):
+    """Erreur lors d'une opération d'export/publication."""
