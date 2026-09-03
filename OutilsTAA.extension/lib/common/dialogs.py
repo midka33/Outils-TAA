5322ac7c7f59073d2fdc782f1a43bc28c4653e2c
midka33 <akdim@taa.archi>
2026-09-03 11:19:53 +0200

    refactor: add common dialogs contract

---
+++ OutilsTAA.extension/lib/common/dialogs.py
@@ -0,0 +1,7 @@
+"""Point d'entrée commun pour les dialogues utilisateur."""
+
+
+def show_error(message, title="Outils TAA"):
+    """Affiche une erreur utilisateur via l'adaptateur UI configuré."""
+    # L'intégration exacte avec pyRevit sera réalisée dans l'implémentation UI.
+    raise NotImplementedError(message)
