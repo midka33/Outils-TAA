0fa90448da9fbc2171290d6797b3f0c249597675
midka33 <akdim@taa.archi>
2026-09-03 12:20:51 +0200

    Export: enrichir le modèle PublicationSet

--- OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_set.py
+++ OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_set.py
@@ -5,9 +5,23 @@ class PublicationSet(object):
     """Conteneur logique d'éléments publiables."""
 
     def __init__(self, name, items=None, source=None, output_directory=None,
-                 filename_template_id=None):
+                 filename_template_id=None, set_id=None, persistent=False):
+        self.id = set_id
         self.name = name
         self.items = list(items or [])
         self.source = source
         self.output_directory = output_directory
         self.filename_template_id = filename_template_id
+        self.persistent = persistent
+
+    def add_item(self, item):
+        """Ajoute un élément au carnet s'il n'est pas déjà présent."""
+        if item is None:
+            return False
+
+        for existing in self.items:
+            if existing.unique_id == item.unique_id:
+                return False
+
+        self.items.append(item)
+        return True
