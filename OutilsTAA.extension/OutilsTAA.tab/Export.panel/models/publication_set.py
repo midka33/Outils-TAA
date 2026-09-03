f2e986fc20a42894026a9379326c2c251ea830e3
midka33 <akdim@taa.archi>
2026-09-03 18:03:31 +0200

    Export: ajouter dossier et réglages persistants aux carnets

--- OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_set.py
+++ OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_set.py
@@ -6,7 +6,8 @@ class PublicationSet(object):
     """Conteneur logique d'éléments publiables."""
 
     def __init__(self, name, items=None, source=None, output_directory=None,
-                 filename_template_id=None, set_id=None, persistent=False):
+                 filename_template_id=None, set_id=None, persistent=False,
+                 folder_id=None, publication_settings=None):
         self.id = set_id
         self.name = name
         self.items = list(items or [])
@@ -14,6 +15,10 @@ class PublicationSet(object):
         self.output_directory = output_directory
         self.filename_template_id = filename_template_id
         self.persistent = persistent
+        # Dossier parent dans l'arborescence Export.
+        self.folder_id = folder_id
+        # Réglages persistants du carnet (PDF/DWG/destination/etc.).
+        self.publication_settings = publication_settings
 
     def add_item(self, item):
         """Ajoute un élément au carnet s'il n'est pas déjà présent."""
