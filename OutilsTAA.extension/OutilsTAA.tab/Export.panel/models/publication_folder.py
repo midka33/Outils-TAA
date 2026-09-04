0d6fd5494f0a729899dbedb07bd6d51091bcc44e
midka33 <akdim@taa.archi>
2026-09-04 11:30:12 +0200

    feat(export): store folder publication settings

--- OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_folder.py
+++ OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_folder.py
@@ -5,8 +5,10 @@
 class PublicationFolder(object):
     """Dossier persistant contenant des carnets de publication."""
 
-    def __init__(self, name, folder_id=None, parent_id=None, persistent=True):
+    def __init__(self, name, folder_id=None, parent_id=None, persistent=True,
+                 publication_settings=None):
         self.id = folder_id
         self.name = name
         self.parent_id = parent_id
         self.persistent = persistent
+        self.publication_settings = publication_settings
