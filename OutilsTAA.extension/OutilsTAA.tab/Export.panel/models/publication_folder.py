1be53ac4a76fcf243f9acd37c5de24bb6c60f6df
midka33 <akdim@taa.archi>
2026-09-16 10:51:55 +0200

    feat(Export): mémoriser l'ordre des dossiers

--- OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_folder.py
+++ OutilsTAA.extension/OutilsTAA.tab/Export.panel/models/publication_folder.py
@@ -6,12 +6,13 @@ class PublicationFolder(object):
     """Dossier persistant contenant des carnets de publication."""
 
     def __init__(self, name, folder_id=None, parent_id=None, persistent=True,
-                 publication_settings=None):
+                 publication_settings=None, sort_order=0):
         self.id = folder_id
         self.name = name
         self.parent_id = parent_id
         self.persistent = persistent
         self.publication_settings = publication_settings
+        self.sort_order = sort_order
 
 
 def _folder_targets(window, folder):
